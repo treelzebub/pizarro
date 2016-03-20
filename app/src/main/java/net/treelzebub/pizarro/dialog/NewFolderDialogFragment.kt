f834cc959f72d4bfeb97194f478f9ab736d90bc1
Tre Murillo <treelzebub@gmail.com>
2016-03-20 13:21:36 -0700

    i guess don't use notNull delegate for OnNewFolderListener

--- app/src/main/java/net/treelzebub/pizarro/dialog/NewFolderDialogFragment.kt
+++ app/src/main/java/net/treelzebub/pizarro/dialog/NewFolderDialogFragment.kt
@@ -7,17 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
-import butterknife.bindView
 import net.treelzebub.pizarro.R
 import net.treelzebub.pizarro.activity.OnNewFolderListener
-import kotlin.properties.Delegates
 
 /**
  * Created by Tre Murillo on 3/20/16
  */
 class NewFolderDialogFragment : DialogFragment() {
 
-    var listener: OnNewFolderListener by Delegates.notNull()
+    var listener: OnNewFolderListener? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_new_folder, container)
@@ -28,7 +26,7 @@ class NewFolderDialogFragment : DialogFragment() {
             if (folder.isNullOrBlank()) {
                 Toast.makeText(context, "Folder name cannot be blank.", Toast.LENGTH_SHORT).show()
             } else {
-                listener.onNewFolder(folder)
+                listener?.onNewFolder(folder)
                 dismiss()
             }
         }
