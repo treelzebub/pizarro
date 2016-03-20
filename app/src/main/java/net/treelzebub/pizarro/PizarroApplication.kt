dfdfad50cb6452f2cbe1a4e517939f5070c70cf2
Tre Murillo <treelzebub@gmail.com>
2016-03-19 18:33:27 -0700

    sharedprefs

--- app/src/main/java/net/treelzebub/pizarro/PizarroApplication.kt
+++ app/src/main/java/net/treelzebub/pizarro/PizarroApplication.kt
@@ -7,8 +7,13 @@ import android.app.Application
  */
 class PizarroApplication : Application() {
 
+    companion object {
+        const val PREFS_FILE = "pizarro_prefs"
+    }
+
     override fun onCreate() {
         super.onCreate()
         BaseInjection.context = this
+        BaseInjection.prefs = getSharedPreferences(PREFS_FILE, MODE_PRIVATE)
     }
 }
